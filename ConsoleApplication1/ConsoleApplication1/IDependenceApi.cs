c9521a3a161d62960d0a356144e928410a580a19
Guy Shachar <ggguuyyy@hotmail.com>
2013-01-08 17:07:44 +0200

    Added files

---
+++ ConsoleApplication1/ConsoleApplication1/IDependenceApi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public interface IDependenceApi
+    {
+        string SomeApi();
+    }
+    
+    public class DependenceApi : IDependenceApi
+    {
+        public string SomeApi()
+        {
+            return "DependenceApi->SomeApi";
+        }
+    }
+
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,41 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //application starts...
+            var container = new WindsorContainer();
+
+            // adds and configures all components using WindsorInstallers from executing assembly
+            container.Install(FromAssembly.This());
+
+            // instantiate and configure root component and all its dependencies and their dependencies and...
+            var someApi = container.Resolve<ISomeApi>();
+            var str = someApi.TheBestMethod();
+
+            System.Console.Write(str);
+            // clean up, application exits
+            container.Dispose();
+            System.Console.ReadKey();
+        }
+    }
+
+    public class SomeApiInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Classes.FromThisAssembly()
+                                   .Where(Component.IsInSameNamespaceAs<SomeApi>())
+                                   .WithService.DefaultInterfaces()
+                                   .LifestyleTransient());
+
+        }
+    }
+}
